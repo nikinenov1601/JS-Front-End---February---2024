d7971087a9817545364ef0f715e75e71ee1fd3f1
nikinenov1601 <nikinenov1@abv.bg>
2024-03-03 21:40:19 +0200

    Adding sixth problem solution to JS arrays - Lab

---
+++ 03-JS-Arrays-And-Strings–Lab/06-Count-String-Occurrences/06-Count-String-Occurrences.js
@@ -0,0 +1,16 @@
+function solve(arr, searchedWord){
+    let words = arr.split(' ');
+    let counter = 0;
+
+    for(let word of words){
+        if(word == searchedWord){
+            counter++;
+        }
+    }
+
+    console.log(counter);
+}
+
+solve('This is a word and it also is a sentence',
+'is'
+);
