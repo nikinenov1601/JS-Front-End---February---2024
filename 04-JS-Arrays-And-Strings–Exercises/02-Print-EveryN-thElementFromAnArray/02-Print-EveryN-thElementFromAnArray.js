d527fc44925a842e30aecb0f2a14aede4fa12345
nikinenov1601 <nikinenov1@abv.bg>
2024-03-04 16:31:32 +0200

    Create 02-Print-EveryN-thElementFromAnArray.js

---
+++ 04-JS-Arrays-And-Strings–Exercises/02-Print-EveryN-thElementFromAnArray/02-Print-EveryN-thElementFromAnArray.js
@@ -0,0 +1,17 @@
+function solve(array, step) {
+    const result = [];
+    
+    for (let i = 0; i < array.length; i += step) {
+        result.push(array[i]);
+    }
+    
+    return result;
+}
+
+solve(['5',
+    '20',
+    '31',
+    '4',
+    '20'],
+    2
+)
