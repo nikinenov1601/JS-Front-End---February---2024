99acd1fada33a1f2b930bbdda4e490f00f597069
nikinenov1601 <nikinenov1@abv.bg>
2024-03-05 20:11:24 +0200

    Adding second problem solution to Functions And Statements-Lab

---
+++ 05-Functions-And-Statements-Lab/02-Math-Power/02-math-power.js
@@ -0,0 +1,13 @@
+function solve(firstNumber, secondNumber) {
+
+    const result = solution(firstNumber, secondNumber);
+    console.log(result);
+
+    function solution(firstNumber, secondNumber){
+
+        return (Math.pow(firstNumber, secondNumber));
+    }
+
+}
+
+solve(2, 8);
